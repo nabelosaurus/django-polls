9019befc0c51da3d269d76dcf145041c7568a99f
Nael Shaban <nael.shaban+github@gmail.com>
2016-05-12 10:22:56 +0300

    Expand on views and matching templates to accommodate voting

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,7 @@
-from django.http import Http404
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import get_object_or_404, render
+
 
 from . models import Question
 
@@ -20,9 +22,24 @@ def detail(request, question_id=1):
     return render(request, 'polls/detail.html', context)
 
 def results(request, question_id=1):
-    context = {}
+    question = get_object_or_404(Question, pk=question_id)
+    context = {
+        'question': question,
+    }
     return render(request, 'polls/results.html', context)
 
 def vote(request, question_id=1):
-    context = {}
-    return render(request, 'polls/vote.html', context)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        context = {
+            'question': question,
+            'error_message': "You didn't select a choice",
+        }
+        return render(request, 'polls/vote.html', context)
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
