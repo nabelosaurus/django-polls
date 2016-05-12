0b1ee675629923cbaa35bde60c8d0391854f20cb
Nael Shaban <nael.shaban+github@gmail.com>
2016-05-12 05:10:37 +0300

    Create placeholder views for polls app. Route urls.

--- polls/views.py
+++ polls/views.py
@@ -6,3 +6,11 @@ from django.shortcuts import render
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
+def detail(request, question_id=1):
+    return HttpResponse("You're looking at the question %s." % question_id)
+
+def results(request, question_id=1):
+    return HttpResponse("You're looking at the results of question %s." % question_id)
+
+def vote(request, question_id=1):
+    return HttpResponse("Hello, world. You're voting on question %s." % question_id )
